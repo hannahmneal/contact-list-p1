ec8f199e18da768006f294b29dc64d89d6023783
Hannah Neal <hannahmneal@gmail.com>
2018-12-28 01:48:25 -0600

    added styling to main.css

--- src/scripts/contactForm.js
+++ src/scripts/contactForm.js
@@ -6,5 +6,19 @@ console.log("contactForm.js");
 //                              TO DO:
 // Create a form with contact input fields and a submit button
     // Optional feature: add an edit button to modify an existing contact
+// -----------------------------------------------------------------------------
 
+//                              BEGIN FORM
+
+let createForm = document.getElementById("display-container");
+createForm.innerHTML= `
+<input id="first-name-input" type="text" placeholder="First Name">
+<input id="last-name-input" type="text" placeholder="Last Name">
+<input id="phone-input" type="text" placeholder="Phone">
+<button id="submit-form-btn">Submit</button>`
+
+
+
+
+//                               END FORM
 
